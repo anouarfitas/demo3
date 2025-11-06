a2d4191cf04d0691d06a197393008f45d9abacfa
anouarfitas <anouar.fitas2007@gmail.com>
2025-11-06 13:54:45 -0500

    yallah

--- src/main/java/org/example/demo3/HelloApplication.java
+++ src/main/java/org/example/demo3/HelloApplication.java
@@ -3,6 +3,7 @@ package org.example.demo3;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,6 +13,9 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+
+        Carre carre = new Carre(20,10, Color.BLACK);
+
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
